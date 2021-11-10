70eeea604d7370107f61287d88be2e4cc98f3513
geer26 <gergo.kurucz@gmail.com>
2021-11-10 11:18:05 +0100

    v1.0

--- srv6/sessioner/__init__.py
+++ srv6/sessioner/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 class Sess():
 
     def __init__(self, app, timeout=3600):
@@ -11,11 +13,29 @@ class Sess():
 
 
     def add_to_list(self, session):
-        pass
+        session['expiration'] = datetime.now() + timedelta(seconds=self.timeout)
+        self.list.append(session)
+        return True
 
 
     def check_on_list(self, session):
-        pass
+        if session in self.sessionlist:
+            return True
+        return False
+
+
+    def check_expired(self, session):
+        if 'expirqtion' not in session.keys():
+            return False
+        if datetime.now() > session['expiration']:
+            return False
+        else:
+            return True
+
+
+    def update_expiration(self, session):
+        session['expiration'] = datetime.now() + timedelta(seconds=self.timeout)
+        return True
 
 
     def get_users(self):
