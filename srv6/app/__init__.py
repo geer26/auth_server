951cea6b0342f98ce7eb6077475ab0b91df7d638
geer26 <gergo.kurucz@gmail.com>
2021-11-10 10:14:11 +0100

    v1.0

--- srv6/app/__init__.py
+++ srv6/app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from fernet import Secret
+from sessioner import Sess
 from flask_restful import Api
 from logger import Logger
 from flask_cors import CORS
@@ -33,6 +34,9 @@ secret = Secret(app)
 
 
 logger = Logger(app)
+
+
+session_handler = Sess(app)
 
 
 start_time = datetime.now()
---
+++ srv6/sessioner/__init__.py
@@ -0,0 +1,28 @@
+class Sess():
+
+    def __init__(self, app, timeout=3600):
+        self.app = app
+        self.sessionlist = []
+        self.timeout = timeout
+
+
+    def remove_from_list(self, session):
+        pass
+
+
+    def add_to_list(self, session):
+        pass
+
+
+    def check_on_list(self, session):
+        pass
+
+
+    def get_users(self):
+        pass
+
+
+    def renew_timeout(self, session):
+        pass
+
+
